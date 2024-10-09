2713750e21aa6cd30eb4605f4101b3018f673127
Tousif Rehman <tousifrehman.ise.rymec@gmail.com>
2024-10-09 12:29:11 +0530

    update layout

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuicon.onclick = () => {
+    menuicon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuicon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
 function handleUserVisit() {
     const lastVisit = localStorage.getItem("lastVisit");
     const currentTime = new Date().getTime();
@@ -62,7 +97,7 @@ function hidePopup() {
 
 // Background animation for color boxes
 const bganimation = document.getElementById('bganimation');
-const numofcolorbox = 400;
+const numofcolorbox = 600;
 
 for (let i = 0; i < numofcolorbox; i++) {
     const colorbox = document.createElement('div');
